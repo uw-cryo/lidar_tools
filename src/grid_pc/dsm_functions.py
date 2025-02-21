ae9016477f3a8f81e436b85694136c0f0328ac63
ShashankBice <sbaglapl@uw.edu>
2025-02-21 17:30:13 +0000

    add functionality to mosaic

--- src/grid_pc/dsm_functions.py
+++ src/grid_pc/dsm_functions.py
@@ -6,6 +6,9 @@ from pyproj import CRS
 from shapely.geometry import Polygon
 import geopandas as gpd
 import requests
+import subprocess
+import ast
+from shutil import which
 
 def return_readers(input_aoi,
                    src_crs,
@@ -197,4 +200,106 @@ def create_dem_stage(dem_filename='dem_output.tif', pointcloud_resolution=1.,
         })
 
     return [dem_stage]
+
+def dem_mosaic(img_list,outfn,tr=None,tsrs=None,stats=None,tile_size=None,extent=None):
+    """
+    From https://github.com/uw-cryo/skysat_stereo/blob/master/skysat_stereo/asp_utils.py
+    mosaic  input image list using ASP's dem_mosaic program.
+    See dem_mosaic documentation here: https://stereopipeline.readthedocs.io/en/latest/tools/dem_mosaic.html
+    Parameters
+    ----------
+    img_list: list
+        List of input images to be mosaiced
+    outfn: str
+        Path to output mosaicked image
+    tr: float/int
+        target resolution of output mosaic
+    t_srs: str
+        target projection of output mosaic (default: EPSG:4326)
+    stats: str
+        metric to use for mosaicing
+    tile_size: int
+        tile size for distributed mosaicing (if less on memory)
+    Returns
+    ----------
+    out: str
+        dem_mosaic log
+    """
+
+    dem_mosaic_opt = []
+  
+    if stats:
+        dem_mosaic_opt.extend(['--{}'.format(stats)])
+    if (tr is not None) & (ast.literal_eval(tr) is not None):
+        dem_mosaic_opt.extend(['--tr', str(tr)])
+    if tsrs:
+        dem_mosaic_opt.extend(['--t_srs', tsrs])
+    if extent:
+        xmin,ymin,xmax,ymax = extent.split(' ')
+        dem_mosaic_opt.extend(['--t_projwin', xmin,ymin,xmax,ymax])
+    dem_mosaic_args = img_list
+    if tile_size:
+        # will first perform tile-wise vertical mosaicing
+        # then blend the result
+        dem_mosaic_opt.extend(['--tile-size',str(tile_size)])
+        temp_fol = os.path.splitext(outfn)[0]+'_temp'
+        dem_mosaic_opt.extend(['-o',os.path.join(temp_fol,'run')])
+        out_tile_op = run_cmd('dem_mosaic',dem_mosaic_args+dem_mosaic_opt)
+        # query all tiles and then do simple mosaic
+        #print(os.path.join(temp_fol,'run-*.tif'))
+        mos_tile_list = sorted(glob.glob(os.path.join(temp_fol,'run-*.tif')))
+        print(f"Found {len(mos_tile_list)}")
+        # now perform simple mosaic
+        dem_mos2_opt = []
+        dem_mos2_opt.extend(['-o',outfn])
+        dem_mos2_args = mos_tile_list
+        out_fn_mos = run_cmd('dem_mosaic',dem_mos2_args+dem_mos2_opt)
+        out = out_tile_op+out_fn_mos
+        print("Deleting tile directory")
+        shutil.rmtree(temp_fol)
+
+    else:
+        # process all at once, no tiling
+        dem_mosaic_opt.extend(['-o',outfn])
+        out = run_cmd('dem_mosaic',dem_mosaic_args+dem_mosaic_opt)
+    return out
+
+def run_cmd(bin, args, **kw):
+    """
+    From https://github.com/uw-cryo/skysat_stereo/blob/master/skysat_stereo/asp_utils.py
+    wrapper around subprocess function to excute bash commands
+    Parameters
+    ----------
+    bin: str
+        command to be excuted (e.g., stereo or gdalwarp)
+    args: list
+        arguments to the command as a list
+    Retuns
+    ----------
+    out: str
+        log (stdout) as str if the command executed, error message if the command failed
+    """
+    
+    #from dshean/vmap.py
+    
+    binpath = which(bin)
+    #if binpath is None:
+        #msg = ("Unable to find executable %s\n"
+        #"Install ASP and ensure it is in your PATH env variable\n"
+       #"https://ti.arc.nasa.gov/tech/asr/intelligent-robotics/ngt/stereo/" % bin)
+        #sys.exit(msg)
+    #binpath = os.path.join('/opt/StereoPipeline/bin/',bin)
+    call = [binpath,]
+    if args is not None: 
+        call.extend(args)
+    #print(call)
+    try:
+        out = subprocess.run(call,check=True,capture_output=True,encoding='UTF-8').stdout
+    except:
+        out = "the command {} failed to run, see corresponding asp log".format(call)
+    return out
+
+
+    
+    
 
